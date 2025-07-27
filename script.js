5ba8425c80b99d608f398d068a6f0e2fda2f1607
Abdalla <abdallaaymn10@gmail.com>
2025-07-27 21:28:45 +0200

    Added functionality to clear button

--- script.js
+++ script.js
@@ -19,11 +19,9 @@ const calculate = function(){
     const secondIndex = display.textContent.indexOf(" ", firstIndex + 1);
     const firstNumber = Number(display.textContent.slice(0, firstIndex));
     const secondNumber = Number(display.textContent.slice(secondIndex + 1));
-    console.log(firstNumber)
-    console.log(secondNumber)
+    const currentOperator = display.textContent.slice(firstIndex + 1, firstIndex + 2)
 
     display.textContent = operate(firstNumber, currentOperator, secondNumber);
-    console.log(display.textContent)
 };
 
 let number1;
@@ -47,7 +45,8 @@ const operate = function(n1, operator, n2) {
 const display = document.querySelector("#display")
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
-const equal = document.querySelector(".equal");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear")
 
 numbers.forEach(number => {
     number.addEventListener("click", () => {
@@ -60,13 +59,13 @@ const chars = ["+", "-", "*", "/"]
 let currentOperator;
 operators.forEach(operator => {
     operator.addEventListener("click", () => {
-        if (chars.some(char => display.textContent.includes(char))){
-            calculate();
-
-        }
+        if (chars.some(char => display.textContent.includes(char))) calculate();
         display.textContent += " " + operator.textContent + " ";
-        currentOperator = operator.textContent;
     })
 })
 
 equal.addEventListener("click", () => calculate());
+
+clear.addEventListener("click", () => {
+    display.textContent = "";
+})
