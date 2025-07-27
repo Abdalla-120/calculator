37f1212a55a26bbccea1160f7d90cfe0fc5204f4
Abdalla <abdallaaymn10@gmail.com>
2025-07-27 17:04:27 +0200

    Addition of operator function and basic HTML calculator

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const multiply = function(n1, n2) {
 const divide = function(n1, n2) {
     return n1 / n2;
 }
+
+let number1;
+let operator;
+let number2;
+
+const operate = function(n1, operator, n2) {
+    if (operator === "+") {
+        add(n1, n2)
+    } else if (operator === "-") {
+        subtract(n1, n2)
+    } else if (operator === "*") {
+        multiply(n1, n2)
+    } else if (operator === "/") {
+        divide(n1, n2)
+    }
+}
