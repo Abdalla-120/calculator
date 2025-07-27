b382c83b5fc0f69d78b1b83bb3be6e6332288458
Abdalla <abdallaaymn10@gmail.com>
2025-07-27 21:19:16 +0200

    Initial commit of index.html, style.css and script.js

--- script.js
+++ script.js
@@ -14,18 +14,53 @@ const divide = function(n1, n2) {
     return n1 / n2;
 }
 
+const calculate = function(){
+    const firstIndex = display.textContent.indexOf(" ");
+    const secondIndex = display.textContent.indexOf(" ", firstIndex + 1);
+    const firstNumber = Number(display.textContent.slice(0, firstIndex));
+    const secondNumber = Number(display.textContent.slice(secondIndex + 1));
+    console.log(firstNumber)
+    console.log(secondNumber)
+
+    display.textContent = operate(firstNumber, currentOperator, secondNumber);
+    console.log(display.textContent)
+};
+
 let number1;
 let operator;
 let number2;
 
 const operate = function(n1, operator, n2) {
+    let result;
     if (operator === "+") {
-        add(n1, n2)
+        result = add(n1, n2)
     } else if (operator === "-") {
-        subtract(n1, n2)
+        result = subtract(n1, n2)
     } else if (operator === "*") {
-        multiply(n1, n2)
+        result = multiply(n1, n2)
     } else if (operator === "/") {
-        divide(n1, n2)
+        result = divide(n1, n2)
     }
+    return result;
 }
+
+const display = document.querySelector("#display")
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        display.textContent += number.textContent
+    });
+});
+
+let currentOperator;
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        display.textContent += " " + operator.textContent + " ";
+        currentOperator = operator.textContent;
+    })
+})
+
+equal.addEventListener("click", () => calculate());
